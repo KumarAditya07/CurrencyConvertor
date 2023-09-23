048e256be2187d5e1b908ee5ba3eb3410459f76d
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2023-09-23 13:00:15 +0530

    debug issue with select currency completed project

--- src/App.jsx
+++ src/App.jsx
@@ -38,32 +38,42 @@ function App() {
                     <form
                         onSubmit={(e) => {
                             e.preventDefault();
+                            convert();
                            
                         }}
                     >
                         <div className="w-full mb-1">
-                            <InputBox
-                                label="From"
-                                
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setFrom(currency)}
+                selectCurrency={from}
+                onAmountChange={(amount) => setAmount(amount)}
                             />
                         </div>
                         <div className="relative w-full h-0.5">
                             <button
                                 type="button"
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                                
+                                onClick={swap}
                             >
                                 swap
                             </button>
                         </div>
                         <div className="w-full mt-1 mb-4">
-                            <InputBox
-                                label="To"
-                                
-                            />
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={to}
+                amountDisable
+
+              />
                         </div>
                         <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-                            Convert 
+                            Convert  {from.toUpperCase()} to {to.toUpperCase()}
                         </button>
                     </form>
                 </div>
